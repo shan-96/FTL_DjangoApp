7949ac1981f9013c2fe1134e4694f65b56c4e4fa
Shantanu Patil <shantanu.patil@iongroup.com>
2020-07-07 00:44:57 +0530

    added sample api

--- ftl_site/ftl_app/views.py
+++ ftl_site/ftl_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import FTL_User
 
 
 def index(request):
-    return HttpResponse("This is a basic test")
+    users = FTL_User.objects.order_by('real_name')
+    output = ", ".join([u.real_name for u in users])
+    return HttpResponse(output)
+
+
+def results(request):
+    return HttpResponse("This is a API test")
