3e2b970320186099fd541f4dc2bba61de80899c9
Shantanu Patil <shantanu.patil@iongroup.com>
2020-07-06 23:37:19 +0530

    change in schema and created admin

--- ftl_site/ftl_app/admin.py
+++ ftl_site/ftl_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import FTL_User, FTL_User_Activity
 
-# Register your models here.
+admin.site.register(FTL_User)
+admin.site.register(FTL_User_Activity)
--- ftl_site/ftl_app/models.py
+++ ftl_site/ftl_app/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 
 class FTL_User(models.Model):
-    id = models.CharField(max_length=200, unique=True)
+    user_id = models.CharField(max_length=200, primary_key=True)
     real_name = models.CharField(max_length=200)
     tz = models.CharField(max_length=100)
-    # pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        obj = "User\nid = " + str(self.user_id) + " real_name = " + str(self.real_name) + " tz = " + str(self.tz)
+        return obj
 
 
 class FTL_User_Activity(models.Model):
-    id = models.ForeignKey(FTL_User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(FTL_User, on_delete=models.CASCADE)
     start_time = models.DateTimeField('start_time')
     end_time = models.DateTimeField('end_time')
+
+    def __str__(self):
+        obj = "User_Activity\nid = " + str(self.user_id) + " start_time = " + str(self.start_time) + " end_time = " + str(
+            self.end_time)
+        return obj
