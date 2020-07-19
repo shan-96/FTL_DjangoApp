f7e2ed615c2b7102c0beffc4295f6812d61efcbc
Shantanu Patil <shantanu.patil@iongroup.com>
2020-07-19 18:27:35 +0530

    added heroku compatibility

--- ftl_site/ftl_app/tests.py
+++ ftl_site/ftl_app/tests.py
@@ -1,10 +1,10 @@
 import datetime
-
+import json
 from django.test import Client
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import FTL_User_Activity
+from .models import FTL_User_Activity, FTL_User
 
 
 class UserActivityTests(TestCase):
@@ -22,6 +22,50 @@ class UserActivityTests(TestCase):
 
 
 class ResultsViewTest(TestCase):
+    def setUp(self):
+        json_str = ''''{
+	"ok": true,
+	"members": [{
+			"id": "W012A3CDE",
+			"real_name": "Egon Spengler",
+			"tz": "America/Los_Angeles",
+			"activity_periods": [{
+					"start_time": "Feb 1 2020  1:33PM",
+					"end_time": "Feb 1 2020 1:54PM"
+				},
+				{
+					"start_time": "Mar 1 2020  11:11AM",
+					"end_time": "Mar 1 2020 2:00PM"
+				},
+				{
+					"start_time": "Mar 16 2020  5:33PM",
+					"end_time": "Mar 16 2020 8:02PM"
+				}
+			]
+		},
+		{
+			"id": "W07QCRPA4",
+			"real_name": "Glinda Southgood",
+			"tz": "Asia/Kolkata",
+			"activity_periods": [{
+					"start_time": "Feb 1 2020  1:33PM",
+					"end_time": "Feb 1 2020 1:54PM"
+				},
+				{
+					"start_time": "Mar 1 2020  11:11AM",
+					"end_time": "Mar 1 2020 2:00PM"
+				},
+				{
+					"start_time": "Mar 16 2020  5:33PM",
+					"end_time": "Mar 16 2020 8:02PM"
+				}
+			]
+		}
+	]
+}'''
+        self.input_data = json.loads(json_str)
+        FTL_User.save(**self.input_data)
+
     def test_empty_string(self):
         c = Client()
         response = c.post('/ftl_app/results/', {'user_id_list': ''})
@@ -31,3 +75,11 @@ class ResultsViewTest(TestCase):
         c = Client()
         response = c.post('/ftl_app/results/', {'user_id_list': 'abc'})
         self.assertEqual(200, response.status_code)
+
+    def test_request_response_logic(self):
+        c = Client()
+        response = c.post(path='/ftl_app/results/', data={'user_id_list': 'abc'}, follow=True)
+        result = json.loads(response.context['results'])
+        self.assertEqual(True, result['ok'])
+        self.assertEqual(0, len(result['members']))
+        self.assertEqual('', result['error_msg'])
--- ftl_site/ftl_site/settings.py
+++ ftl_site/ftl_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR,'ftl_app/static/ftl_app/')
+
+django_heroku.settings(locals())
